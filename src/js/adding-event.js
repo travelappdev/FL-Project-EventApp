19891009e9a73f6b01bd71b2cddda77de480c75a
TetyanaKandybal <tetyanakandybal@gmail.com>
2016-09-14 15:40:07 +0300

    fixes for home page, logo, hamburger menu

--- src/js/adding-event.js
+++ src/js/adding-event.js
@@ -14,3 +14,19 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+function readURLProfile(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.picture-area')
+                .attr('src', e.target.result)
+                .width(310)
+                .height(322);
+               
+
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
