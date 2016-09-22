422aeee457f3377bfa74424f25122f372396db46
slovechik <yarikmelnyk39@gmail.com>
2016-09-22 10:26:40 +0300

    changed carousel functionality

--- src/js/controllers/generalCtrl.js
+++ src/js/controllers/generalCtrl.js
@@ -1,33 +1,85 @@
 angular.module('mainApp')
-.controller('generalCtrl',['$scope', '$http', '$uibModal', '$sce', function (sc, $http, uibModal, sce){
+  .controller('generalCtrl', ['$scope', '$http', '$uibModal', '$sce', function(sc, $http, uibModal, sce) {
 
-  sc.errorMsg = '';
+    sc.errorMsg = '';
 
 
-// download top events
-// should be fixed
-
-  $http.get('/api/topevents')
-    .then(function(response) {
-      sc.events = angular.fromJson(response.data);
-    });
-
-// post user data on server
-
-  sc.postData = function() {
-    if(sc.user_password === sc.user_repassword) {
-      $http.post('http://localhost:8000/api/users/', {
-        "email": sc.user_email,
-        "username": sc.user_name,
-        "password": sc.user_password
+    // download top events
+    // should be fixed
+    sc.gallery_cells = [{
+      events: [{
+        name: "String1",
+        place: "String",
+        date: "String",
+        time: "Number",
+        type: "String",
+        payment: "String",
+        description: "String"
+      }, {
+        name: "String2",
+        place: "String",
+        date: "String",
+        time: "Number",
+        type: "String",
+        payment: "String",
+        description: "String"
+      }, {
+        name: "String3",
+        place: "String",
+        date: "String",
+        time: "Number",
+        type: "String",
+        payment: "String",
+        description: "String"
+      }]
+    }, {
+      events: [{
+        name: "String4",
+        place: "String",
+        date: "String",
+        time: "Number",
+        type: "String",
+        payment: "String",
+        description: "String"
+      }, {
+        name: "String5",
+        place: "String",
+        date: "String",
+        time: "Number",
+        type: "String",
+        payment: "String",
+        description: "String"
+      }, {
+        name: "String6",
+        place: "String",
+        date: "String",
+        time: "Number",
+        type: "String",
+        payment: "String",
+        description: "String"
+      }]
+    }];
+    $http.get('/api/topevents')
+      .then(function(response) {
+        sc.events = angular.fromJson(response.data);
       });
-    } else {
-      sc.errorMsg = 'Confirmation failed!';
-      sc.user_password = '';
-      sc.user_repassword = '';
-    }
 
-  }
+    // post user data on server
+
+    sc.postData = function() {
+      if (sc.user_password === sc.user_repassword) {
+        $http.post('http://localhost:8000/api/users/', {
+          "email": sc.user_email,
+          "username": sc.user_name,
+          "password": sc.user_password
+        });
+      } else {
+        sc.errorMsg = 'Confirmation failed!';
+        sc.user_password = '';
+        sc.user_repassword = '';
+      }
+
+    }
 
 
     sc.animationsEnabled = true;
@@ -44,8 +96,7 @@ angular.module('mainApp')
 
     var modalInstance;
 
-    sc.open = function (url, size, controller)
-    {
+    sc.open = function(url, size, controller) {
 
       modalInstance = uibModal.open({
         animation: this.animationsEnabled,
@@ -57,10 +108,10 @@ angular.module('mainApp')
         size: size
       });
 
-      sc.openComponentModal = function () {
+      sc.openComponentModal = function() {
         var modalInstance = uibModal.open({
-        animation: this.animationsEnabled,
-        component: 'modalComponent'
+          animation: this.animationsEnabled,
+          component: 'modalComponent'
         });
       };
 
@@ -69,4 +120,4 @@ angular.module('mainApp')
 
 
 
-}]);
+  }]);
