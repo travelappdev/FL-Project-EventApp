92d518036feb8e8ee3e704987b222b4876924c85
Slovechik <yarikmelnyk39@gmail.com>
2016-09-27 01:09:41 +0300

    added sending data to DB

--- src/js/fb_login.js
+++ src/js/fb_login.js
@@ -3,6 +3,8 @@ window.fbAsyncInit = function() {
     FB.init({
         appId: '211283292620723',
         xfbml: true,
+        status: true,
+        cookie: true,
         version: 'v2.7'
     });
 
@@ -25,61 +27,63 @@ window.fbAsyncInit = function() {
 }(document, 'script', 'facebook-jssdk'));
 
 
-
 function fbLogin() {
 
     loginType = 'fb';
+
     FB.login(function(response) {
 
         if (response.status === "connected") {
 
             FB.api('/me', 'GET', {
-                    fields: 'first_name,last_name,id,picture.width(300).height(300)'
+                    fields: 'name,id,picture.width(300).height(300),email,hometown,gender,birthday'
                 },
                 function(response) {
 
-                    console.log(response.id);
-                    console.log(response.first_name);
-                    console.log(response.last_name);
-                    console.log(response.picture.data.url);
+                    var age = Math.floor((Date.now() - Date.parse(response.birthday)) / (1000 * 60 * 60 * 24 * 364.75));
 
-                  })
+                    $.post('http://localhost:8000/api/users/', {
 
+                        "email": response.id+'@com',
+                        "username": response.name,
+                        "userPhoto": response.picture.data.url,
+                        "homeTown": response.hometown.name,
+                        "gender": response.gender,
+                        "age": age,
+                        "password": 'pass'
 
-                    //
-                    // $.post( "http://localhost:8000/api/users/", {
-                    //    email: `${response.id}`,
-                    //    username: `${response.first_name}_${response.last_name}`,
-                    //    userPhoto: `${response.picture.data.url}`
-                    //  })
-                    //  .done(function( data ) {
-                    //    console.log('data',data.username);
-                    //  }, "json");
+                    });
 
+                    $.get(`http://localhost:8000/api/users/${response.id}@com`, function(resp) {
+
+                        document.cookie = `email=${resp.email}`;
+                        document.cookie = `password=${resp.password}`;
+                        document.cookie = `username=${resp.username}`;
+                        document.cookie = `age=${resp.age}`;
+                        document.cookie = `phone=${resp.phone}`;
+                        document.cookie = `homeTown=${resp.homeTown}`;
+                        document.cookie = `gender=${resp.gender}`;
+                        document.cookie = `subscribed=${resp.subscribed}`;
+                        document.cookie = `createdEvents=${resp.createdEvents}`;
+                        document.cookie = `userPhoto=${resp.userPhoto}`;
+
+                    });
+
+                    /*console.log(response.hometown.name);
+                    console.log(response.gender);
+                    console.log(age);
+                    console.log(response.name);
+                    console.log(response.picture.data.url);*/
+                })
 
         } else if (response.status === "not_authorized") {
             console.log("You are not logged in.");
         } else {
-            console.log("You are not in facebook.");
+            console.log("You are not logged in facebook.");
         }
-
     });
 }
 
-
-function fbGetInfo() {
-
-    FB.api('/me', 'GET', {
-            fields: 'first_name,last_name,id,picture.width(300).height(300)'
-        },
-        function(response) {
-            console.log(response.id);
-            console.log(response.first_name);
-            console.log(response.last_name);
-            console.log(response.picture.data.url);
-        })
-
-}
 
 
 function fbLogout() {
@@ -112,3 +116,5 @@ function shareFB(name, picture, caption, description) {
 
 
 }
+
+
